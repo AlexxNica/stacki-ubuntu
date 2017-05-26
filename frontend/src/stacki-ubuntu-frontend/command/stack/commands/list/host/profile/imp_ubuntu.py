c61960226954214df1da8697461f7e81f5bbb9e7
Aish <cyberaishu@gmail.com>
2017-05-26 16:55:55 -0700

    Updated ubuntu preseed generation code to be in line with Mason's changes in stacki codebase. Got rid off lot of the old profile generation code.

--- frontend/src/stacki-ubuntu-frontend/command/stack/commands/list/host/profile/imp_ubuntu.py
+++ frontend/src/stacki-ubuntu-frontend/command/stack/commands/list/host/profile/imp_ubuntu.py
@@ -1,33 +1,33 @@
 #! /opt/stack/bin/python
 #
 # @SI_Copyright@
-#                             www.stacki.com
+#                               www.stacki.com
 #                                  v3.0
-# 
+#
 #      Copyright (c) 2006 - 2015 StackIQ Inc. All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
 # met:
-#  
+#
 # 1. Redistributions of source code must retain the above copyright
 # notice, this list of conditions and the following disclaimer.
-#  
+#
 # 2. Redistributions in binary form must reproduce the above copyright
 # notice unmodified and in its entirety, this list of conditions and the
-# following disclaimer in the documentation and/or other materials provided 
+# following disclaimer in the documentation and/or other materials provided
 # with the distribution.
-#  
+#
 # 3. All advertising and press materials, printed or electronic, mentioning
-# features or use of this software must display the following acknowledgement: 
-# 
-# 	 "This product includes software developed by StackIQ" 
-#  
+# features or use of this software must display the following acknowledgement:
+#
+#        "This product includes software developed by StackIQ"
+#
 # 4. Except as permitted for the purposes of acknowledgment in paragraph 3,
 # neither the name or logo of this software nor the names of its
 # authors may be used to endorse or promote products derived from this
 # software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY STACKIQ AND CONTRIBUTORS ``AS IS''
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
 # THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
@@ -42,116 +42,23 @@
 # @SI_Copyright@
 
 import string
-from xml.sax import saxutils
-from xml.sax import handler
-from xml.sax import make_parser
-from stack.license import *
 import stack.commands
+import stack.gen
 import stack.ubuntu.gen
 
 
-class ProfileHandler(handler.ContentHandler,
-                     handler.DTDHandler,
-                     handler.EntityResolver,
-                     handler.ErrorHandler):
+class Implementation(stack.commands.list.host.profile.implementation):
+        
+        def generator(self):
+                return stack.ubuntu.gen.Generator()
 
-	def __init__(self):
-		handler.ContentHandler.__init__(self)
-                self.recording = False
-                self.text      = ''
-                self.chapters  = {}
-                self.chapter   = None
-
-	def startElement(self, name, attrs):
-                if name == 'chapter':
-                        self.chapter   = self.chapters[attrs.get('name')] = []
-                        self.recording = True
-
-	def endElement(self, name):
-                if self.recording:
-                        self.chapter.append(self.text)
-                        self.text = ''
-
-                if name == 'chapter':
-                        self.recording = False
-
-	def characters(self, s):
-                if self.recording:
-                        self.text += s
-
-        def getChapter(self, chapter):
-                doc = []
-                if chapter in self.chapters:
-                        for text in self.chapters[chapter]:
-                        	doc.append(text.strip())
-                return doc
-
-
-class Implementation(stack.commands.Implementation):
-
-
-	@licenseCheck
-	def run(self, args):
-
-                host        = args[0]
-                xmlinput    = args[1]
-                profileType = args[2]
-                isDocument  = args[3]
-                profile     = []
-		generator   = stack.ubuntu.gen.Generator()
-
-                generator.setProfileType(profileType)
-		generator.parse(xmlinput)
-
-                profile.append('<?xml version="1.0" standalone="no"?>')
-                profile.append('<profile-%s os="ubuntu">' % generator.getProfileType())
-		profile.append('<chapter name="meta">')
-                profile.append('\t<section name="order">')
-                for line in generator.generate('order'):
-	                profile.append('%s' % line)
-                profile.append('\t</section>')
-                profile.append('\t<section name="debug">')
-                for line in generator.generate('debug'):
-                        profile.append(line)
-                profile.append('\t</section>')
+        def chapter(self, generator, profile):
+		profile.append('<chapter name="preseed">')
+                for section in [ 'main', 'post', 'finish' ]:
+                        profile.append('\t<section name="%s">' % section)
+                        for line in generator.generate(section):
+                                profile.append(line)
+                        profile.append('\t</section>')
 		profile.append('</chapter>')
 
-                if generator.getProfileType() == 'native':
-                        profile.append('<chapter name="preseed">')
-                        for section in [ 'main',
-                                         'packages',
-                                         'post',
-                                         'finish']:
-                                profile.append('\t<section name="%s">' % section)
-                                for line in generator.generate(section):
-                                        profile.append(line)
-                                profile.append('\t</section>')
-                        profile.append('</chapter>')
 
-                elif generator.getProfileType() == 'shell':
-                        profile.append('<chapter name="bash">')
-                        profile.append('#! /bin/bash')
-                        for section in [ 'packages',
-                                         'post',
-					 'boot' ]:
-                                profile.append('\t<section name="%s">' % section)
-                                for line in generator.generate(section):
-                                        profile.append(line)
-                                profile.append('\t</section>')
-                        profile.append('</chapter>')
-
-
-                profile.append('</profile-%s>' % generator.getProfileType())
-
-                if not isDocument:
-			parser  = make_parser()
-			handler = ProfileHandler()
-
-			parser.setContentHandler(handler)
-                        for line in profile:
-                                parser.feed('%s\n' % line)
-
-                        profile = handler.getChapter('preseed')
-
-                for line in profile:
-                        self.owner.addOutput(host, line)
--- frontend/src/stacki-ubuntu-frontend/pylib/stack/ubuntu/gen.py
+++ frontend/src/stacki-ubuntu-frontend/pylib/stack/ubuntu/gen.py
@@ -92,6 +92,7 @@
 # IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 # @Copyright@
 
+import cStringIO
 import string
 import types
 import sys
@@ -99,92 +100,79 @@ import os
 import time
 import xml.dom.NodeFilter
 import xml.dom.ext.reader.Sax2
-import stack.js
 import stack.cond
 import stack.gen
-	
+
+class StackNodeFilter(stack.gen.StackNodeFilter):
+	pass
+
+class NodeFilter(stack.gen.NodeFilter):
+	def acceptNode(self, node):
+		return self.FILTER_ACCEPT
 		
 class Generator(stack.gen.Generator):
 
 	def __init__(self):
 		stack.gen.Generator.__init__(self)	
-		self.ks                 = {}
-		self.ks['order']	= []
-		self.ks['debug']	= []
-		self.ks['main']         = []
-		self.ks['packages']     = []
-		self.ks['finish']	= []
-		self.ks['pre']          = []
-		self.ks['post']         = []
-		self.finish_section     = 0
-		self.log = '/var/log/stack-install.log'
-
-                # We could set these elsewhere but this is the current
-                # definition of the Ubuntu Generator.
-
-                self.setOS('ubuntu')
+		self.setOS('ubuntu')
 		self.setArch('x86_64')
+		
+		self.doc                = None
+		self.mainSection        = stack.gen.ProfileSection()
+		self.postSection        = stack.gen.ProfileSection()
+		self.finishSection      = stack.gen.ProfileSection()
+
+	##
+	## Render xml for debugging purposes
+	##
+        def render(self, node):
+                stream  = cStringIO.StringIO()
+                xml.dom.ext.PrettyPrint(node, stream)
+                s = stream.getvalue()
+                stream.close()
+                return s
 	
 	##
 	## Parsing Section
 	##
-	
 	def parse(self, xml_string):
 		import cStringIO
 		xml_buf = cStringIO.StringIO(xml_string)
-		doc = xml.dom.ext.reader.Sax2.FromXmlStream(xml_buf)
-		filter = stack.gen.MainNodeFilter(self.attrs)
-		iter = doc.createTreeWalker(doc, filter.SHOW_ELEMENT,
-			filter, 0)
+		self.doc = xml.dom.ext.reader.Sax2.FromXmlStream(xml_buf)
+		
+		filter = NodeFilter(self.attrs)
+		iter   = self.doc.createTreeWalker(self.doc, filter.SHOW_ELEMENT, filter, 0)
 		node = iter.nextNode()
+		
 		while node:
-			if node.nodeName == 'profile':
-				self.handle_profile(node)
-			elif node.nodeName == 'main':
+			if node.localName != 'profile':
+				self.order(node)
+
+			if node.nodeName == 'main':
 				child = iter.firstChild()
 				while child:
 					self.handle_mainChild(child)
 					child = iter.nextSibling()
+			else:
+				name = string.join(node.nodeName.split(':'), '_')
+				try:
+					fn = eval('self.handle_%s' % name)
+				except AttributeError:
+					#print('handle_%s' %name)
+					pass
+			if fn:
+				fn(node)
 			node = iter.nextNode()
-
-		filter = stack.gen.OtherNodeFilter(self.attrs)
-		iter = doc.createTreeWalker(doc, filter.SHOW_ELEMENT, filter, 0)
-		node = iter.nextNode()
-		while node:
-			if node.nodeName != 'profile':
-				self.order(node)
-                                try:
-                                        fn = eval('self.handle_%s' % node.nodeName)
-                                except AttributeError:
-                                        fn = None
-                                if fn:
-                                        fn(node)
-			node = iter.nextNode()
-
-
-	# <profile>
-	
-	def handle_profile(self, node):
-		# pull out the attr to handle generic conditionals
-		# this replaces the old arch/os logic but still
-		# supports the old syntax
-
-		if node.attributes:
-			attrs = node.attributes.getNamedItem((None, 'attrs'))
-			if attrs:
-				dict = eval(attrs.value)
-				for (k,v) in dict.items():
-					self.attrs[k] = v
 
 	def handle_mainChild(self, node):
-		attr = node.attributes
-		roll, nodefile, color = self.get_context(node)
-		if 'tasksel' in node.nodeName:
-			self.ks['main'].append('%s %s' % (node.nodeName, self.getChildText(node)))
+		nodefile = self.getAttr(node, 'file')
+		if 'tasksel' in node.nodeName:	
+			self.mainSection.append('%s %s' % (node.nodeName, self.getChildText(node)), nodefile)
 		else:
-			self.ks['main'].append('d-i %s/%s' % (node.nodeName, self.getChildText(node)))
+			self.mainSection.append('d-i %s/%s' % (node.nodeName, self.getChildText(node)), nodefile)
 	
 	def handle_late_command(self, node):
+		nodefile = self.getAttr(node, 'file')
 		txt = self.getChildText(node)
 		commands = txt.split(";")
 		
@@ -193,132 +181,35 @@ class Generator(stack.gen.Generator):
 			if not command:
 				continue
 			
-			if not self.ks['post']:
-				self.ks['post'].append('d-i preseed/%s string %s;\\' % (node.nodeName.strip(), command))
+			if not self.postSection.generate():
+				self.postSection.append('d-i preseed/%s string %s;\\' % (node.nodeName.strip(), command), nodefile)
 			elif idx < len(commands) - 2:
-				self.ks['post'].append('%s;\\' % command)
+				self.postSection.append('%s;\\' % command, nodefile)
 			else:
-				self.ks['post'].append('%s' % command)
-			
+				self.postSection.append('%s' % command, nodefile)
 
 	def handle_grub_installer(self, node):
-		self.ks['finish'].append('d-i grub-installer/%s' % self.getChildText(node))
+		nodefile = self.getAttr(node, 'file')
+		self.finishSection.append('d-i grub-installer/%s' % self.getChildText(node), nodefile)
 
 	def handle_finish_install(self, node):
-		self.ks['finish'].append('d-i finish-install/%s' % self.getChildText(node))
+		nodefile = self.getAttr(node, 'file')
+		self.finishSection.append('d-i finish-install/%s' % self.getChildText(node), nodefile)
 
 	def handle_pre(self, node):
 		pass
 
-	def handle_package(self, node):
+	def handle_post(self, node):
 		pass
 
-
-		
-	def get_context(self, node):
-		# This function returns the rollname,
-		# and nodefile of the node currently being
-		# processed
-		attr = node.attributes
-		roll = None
-		nodefile = None
-		color = None
-		if attr.getNamedItem((None, 'roll')):
-			roll = attr.getNamedItem((None, 'roll')).value
-		if attr.getNamedItem((None, 'file')):
-			nodefile = attr.getNamedItem((None, 'file')).value
-		if attr.getNamedItem((None, 'color')):
-			color = attr.getNamedItem((None, 'color')).value
-		return (roll, nodefile, color)
+	def handle_package(self, node):
+		pass
 	
-	# <post>
-	def handle_post(self, node):
-		"""Function works in an interesting way. On solaris the post
-		sections are executed in the installer environment rather than
-		in the installed environment. So the way we do it is to write
-		a script for every post section, with the correct interpreter
-		and execute it with a chroot command.
-		"""
-		# TODO remove return later
-		return
-		attr = node.attributes
-		# By default we always want to chroot, unless
-		# otherwise specified
-		if attr.getNamedItem((None, 'chroot')):
-			chroot = attr.getNamedItem((None, 'chroot')).value
-		else:
-			chroot = 'yes'
-
-		# By default, the interpreter is always /bin/sh, unless
-		# otherwise specified.
-		if attr.getNamedItem((None, 'interpreter')):
-			interpreter = attr.getNamedItem((None,
-				'interpreter')).value
-		else:
-			interpreter = '/bin/sh'
-
-		# The args that are supplied are for the command that
-		# you want to run, and not to the installer section.
-		if attr.getNamedItem((None, 'arg')):
-			arg = attr.getNamedItem((None, 'arg')).value
-		else:
-			arg = ''
-
-		list = []
-
-		if self.finish_section == 0:
-			list.append("d-i preseed/late_command string in-target")
-
-		if chroot == 'yes':
-			list.append("\tin-target cat > /a/tmp/post_section_%d << '__eof__'; \\"
-					% self.finish_section)
-			list.append("#!%s" % interpreter)
-			list.append(self.getChildText(node))
-			list.append("__eof__")
-			list.append("\tin-target chmod a+rx /a/tmp/post_section_%d; \\"
-					% self.finish_section)
-			list.append("\tin-target chroot /a /tmp/post_section_%d %s; \\"
-					% (self.finish_section, arg))
-		else:
-			if interpreter is not '/bin/sh':
-				list.append("\tin-target cat > /tmp/post_section_%d "
-					"<< '__eof__'; \\"
-					% self.finish_section)
-				list.append("#!%s" % interpreter)
-				list.append(self.getChildText(node))
-				list.append("__eof__")
-				list.append("\tin-target chmod a+rx /tmp/post_section_%d;"
-					% self.finish_section)
-				list.append("\t%s /tmp/post_section_%d;"
-					% (interpreter, self.finish_section))
-			
-			else:
-				list.append(self.getChildText(node))
-
-		self.finish_section = self.finish_section+1
-		self.ks['finish'] += list
-
 	def generate_main(self):
-		list = []
-		list.append('')
-		list += self.ks['main']
-		return list
-
-	def generate_packages(self):
-		list = []
-		list.append('')
-		list += self.ks['packages']
-		return list
+		return self.mainSection.generate()
 
 	def generate_post(self):
-		list = []
-		list.append('')
-		list += self.ks['post']
-		return list
+		return self.postSection.generate()
 
 	def generate_finish(self):
-		list = []
-		list.append('')
-		list += self.ks['finish']
-		return list
-
+		return self.finishSection.generate()
